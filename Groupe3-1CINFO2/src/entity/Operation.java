7272c4896d71e182e85ce02935a6ea0bd6616153
zied-snoussi <ziedsnoussi.tn@gmail.com>
2024-03-16 02:14:57 +0100

    update project and all fonctionalite

---
+++ Groupe3-1CINFO2/src/entity/Operation.java
@@ -0,0 +1,106 @@
+package entity;
+
+// Class to perform operations on Person objects
+public class Operation {
+    // Array of Person objects
+    private Person[] persons;
+
+    // Getter for persons
+    public Person[] getPersons() {
+        return persons;
+    }
+
+    // Setter for persons
+    public void setPersons(Person[] persons) {
+        this.persons = persons;
+    }
+
+    // Constructor for Operation class
+    public Operation(Person[] persons) {
+        this.persons = persons;
+    }
+
+    // Method to sort persons by name in alphabetical order
+    public Person[] trierParOrdreAlphabetique() {
+        Person[] personnes = this.getPersons();
+        int n = personnes.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (personnes[i].getNom().compareTo(personnes[j].getNom()) > 0) {
+                    Person temp = personnes[i];
+                    personnes[i] = personnes[j];
+                    personnes[j] = temp;
+                }
+            }
+        }
+        return personnes;
+    }
+
+    // Method to sort persons by age in ascending order
+    public Person[] trierParOrdreAge() {
+        Person[] personnes = this.getPersons();
+        int n = personnes.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (personnes[i].getAge() > personnes[j].getAge()) {
+                    Person temp = personnes[i];
+                    personnes[i] = personnes[j];
+                    personnes[j] = temp;
+                }
+            }
+        }
+        return personnes;
+    }
+
+    // Method to sort persons by ID in ascending order
+    public Person[] trierParOrdreCroissant() {
+        Person[] personnes = this.getPersons();
+        int n = personnes.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (personnes[i].getId() > personnes[j].getId()) {
+                    Person temp = personnes[i];
+                    personnes[i] = personnes[j];
+                    personnes[j] = temp;
+                }
+            }
+        }
+        return personnes;
+    }
+
+    // Method to sort persons by ID in descending order
+    public Person[] trierParOrdreDecroissant() {
+        Person[] personnes = this.getPersons();
+        int n = personnes.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (personnes[i].getId() < personnes[j].getId()) {
+                    Person temp = personnes[i];
+                    personnes[i] = personnes[j];
+                    personnes[j] = temp;
+                }
+            }
+        }
+        return personnes;
+    }
+
+    // Method to display all persons
+    public void afficherPersonnes() {
+        Person[] personnes = this.getPersons();
+        for (Person personne : personnes) {
+            System.out.println(personne);
+        }
+    }
+
+    // Method to check if a string is a palindrome
+    public static boolean estPalindrome(String chaine) {
+        chaine = chaine.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int n = chaine.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (chaine.charAt(i)!= chaine.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Groupe3-1CINFO2/src/main/Main.java
@@ -0,0 +1,57 @@
+package main;
+
+import entity.Operation;
+import entity.Person;
+
+public class Main {
+        public static void main(String[] args) {
+                System.out.println(
+                                "\n++++++++++++++++++++++++++++++++++++++++++++Start+++++++++++++++++++++++++++++++++++++++++\n");
+                // Creation of persons with diverse attributes
+                Person p1 = new Person(1, "Ali", "Ahmed", 25, "Tunis", "12345678", "ali.ahmed@gmail.com", "012345678",
+                                "123456789", "Developer", "Active", "I am a developer");
+                Person p2 = new Person(2, "Mohamed", "Mohamed", 30, "Sfax", "87654321", "mohamed.mohamed@gmail.com",
+                                "876543210", "987654321", "Designer", "Inactive", "I am a designer");
+                Person p3 = new Person(3, "Salah", "Ali", 35, "Sousse", "11223344", "salah.ali@gmail.com", "44332211",
+                                "556677889", "Manager", "Active", "I am a manager");
+
+                // Initialize the operation with an array of persons
+                Operation op = new Operation(new Person[] { p1, p2, p3 });
+            
+                // Display the list of persons
+                op.afficherPersonnes();
+                System.out.println(
+                                "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+                // Sort the persons in alphabetical order and display the sorted list
+                System.out.println("Sort by alphabetical order:");
+                op.trierParOrdreAlphabetique();
+                op.afficherPersonnes();
+                System.out.println(
+                                "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+                // Sort the persons by age and display the sorted list
+                System.out.println("Sort by age:");
+                op.trierParOrdreAge();
+                op.afficherPersonnes();
+                System.out.println(
+                                "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+                // Sort the persons in ascending order and display the sorted list
+                System.out.println("Sort in ascending order:");
+                op.trierParOrdreCroissant();
+                op.afficherPersonnes();
+                System.out.println(
+                                "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+                // Sort the persons in descending order and display the sorted list
+                System.out.println("Sort in descending order:");
+                op.trierParOrdreDecroissant();
+                op.afficherPersonnes();
+                System.out.println(
+                                "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+                // Check if various strings are palindromes
+                System.out.println("Is '12321' a palindrome? " + Operation.estPalindrome("12321"));
+                System.out.println("Is '123212' a palindrome? " + Operation.estPalindrome("123212"));
+                System.out.println("Is 'racecar' a palindrome? " + Operation.estPalindrome("racecar"));
+                System.out.println("Is 'hello' a palindrome? " + Operation.estPalindrome("hello"));
+                System.out.println(
+                                "\n++++++++++++++++++++++++++++++++++++++++++++++++++++End+++++++++++++++++++++++++++++++++\n");
+        }
+}
