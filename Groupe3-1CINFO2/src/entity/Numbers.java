3256d0cd9fc3de4330325acc3a30af333b011076
zied-snoussi <ziedsnoussi.tn@gmail.com>
2024-05-01 16:18:59 +0100

    update methid verify of Numbers class

--- Groupe3-1CINFO2/src/entity/Numbers.java
+++ Groupe3-1CINFO2/src/entity/Numbers.java
@@ -1,5 +1,5 @@
 public class Numbers {
-    int calculFactorielle (int x){
+    int calculFactorielle(int x) {
         if (x == 0) {
             return 1;
         } else {
@@ -10,21 +10,21 @@ public class Numbers {
             return res;
         }
     }
-    boolean verifierNombrePair (int n){
-      return n % 2 == 0;
+
+    boolean verifierNombrePair(int n) {
+        return n % 2 == 0;
     }
-    boolean verifierNombrePremier(){
-        if (nombre <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(nombre); i++) {
-            if (nombre % i == 0) {
+
+    boolean verifierNombrePremier() {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
         return true;
     }
-    int CalculPuissance(int a,int b){
+
+    int CalculPuissance(int a, int b) {
         int res = 1;
         for (int i = 0; i < b; i++) {
             res *= a;
